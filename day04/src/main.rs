b8ade8addd9563d2d7134780880decac4dcdfec5
Robert Brons <r.brons@vlife.kz>
2023-12-26 13:50:12 +0600

    Day04 complete

---
+++ day04/src/main.rs
@@ -0,0 +1,69 @@
+use core::num;
+use std::str::FromStr;
+
+fn main() {
+    let input = include_str!("./input.txt");
+    let lines = input.lines();
+    let games: Vec<Game> = lines
+        .into_iter()
+        .map(|line| Game::from_str(line))
+        .filter(|game| game.is_ok())
+        .map(|game| game.unwrap())
+        .collect();
+    let points_sum: usize = games.iter().map(|game| game.calculate_points()).sum();
+    println!("{:?}", points_sum);
+}
+
+#[derive(Debug)]
+struct Game {
+    id: usize,
+    numbers: Vec<usize>,
+    winning_numbers: Vec<usize>,
+}
+
+impl Game {
+    fn calculate_points(&self) -> usize {
+        let mut points = 0;
+        for number in &self.winning_numbers {
+            if self.numbers.contains(&number) {
+                points += 1;
+            }
+        }
+        if points == 0 {
+            0
+        } else {
+            (2 as usize).pow(points - 1)
+        }
+    }
+}
+
+impl FromStr for Game {
+    type Err = ParseGameError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (id_string, numbers_string) = s
+            .strip_prefix("Card ")
+            .and_then(|s| s.split_once(":"))
+            .ok_or(ParseGameError)?;
+        let (scratch_numbers_string, winning_numbers_string) =
+            numbers_string.split_once("|").ok_or(ParseGameError)?;
+
+        let scratch_numbers: Vec<usize> = scratch_numbers_string
+            .trim()
+            .split_whitespace()
+            .map(|e| e.parse::<usize>().unwrap())
+            .collect();
+        let winning_numbers: Vec<usize> = winning_numbers_string
+            .trim()
+            .split_whitespace()
+            .map(|e| e.parse::<usize>().unwrap())
+            .collect();
+        Ok(Game {
+            id: id_string.trim().parse().map_err(|_| ParseGameError)?,
+            numbers: scratch_numbers,
+            winning_numbers,
+        })
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ParseGameError;
