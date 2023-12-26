d23d2fee6f5def7937514b97d7f950a38e7fc0c2
Robert Brons <r.brons@vlife.kz>
2023-12-26 14:09:03 +0600

    Day02a cleanup

--- day02a/src/main.rs
+++ day02a/src/main.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 fn main() {
     let input = include_str!("./input.txt");
-    let split_input: Vec<&str> = input.split("\n").collect();
+    let split_input: Vec<&str> = input.split('\n').collect();
     let mut games = vec![];
     for game in split_input {
         games.push(Game::from_str(game).unwrap());
@@ -59,13 +59,13 @@ struct GameResult {
 impl FromStr for GameResult {
     type Err = ParseGameError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let dice_count: Vec<&str> = s.split(",").map(|s| s.trim()).collect();
+        let dice_count: Vec<&str> = s.split(',').map(|s| s.trim()).collect();
         let mut green: Option<usize> = None;
         let mut blue: Option<usize> = None;
         let mut red: Option<usize> = None;
 
         for dice in dice_count {
-            let mut dice_iter = dice.split(" ");
+            let mut dice_iter = dice.split(' ');
             let count = dice_iter.next().unwrap();
             let colour = dice_iter.next().unwrap();
             if colour.eq("green") {
@@ -109,11 +109,11 @@ impl FromStr for Game {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let (id_string, game_results) = s
             .strip_prefix("Game ")
-            .and_then(|s| s.split_once(":"))
+            .and_then(|s| s.split_once(':'))
             .ok_or(ParseGameError)?;
         let mut game_results_parsed: Vec<GameResult> = vec![];
 
-        for game_result in game_results.split(";").map(|s| s.trim()) {
+        for game_result in game_results.split(';').map(|s| s.trim()) {
             game_results_parsed.push(GameResult::from_str(game_result).unwrap());
         }
 
